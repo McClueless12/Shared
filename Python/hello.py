5b46548ac3010b271f38fa18449ab90279e187cd
Jared Damron <Jared_Damron@live.com>
2018-12-15 02:54:45 -0600

    Added a clock and module imports

--- Python/hello.py
+++ Python/hello.py
@@ -1,4 +1,13 @@
+import time
+from os import system
+
 print("Hello, World!")
 
 print("This is John")
 print("This is Jared")
+
+while True:
+    print("Tick")
+    time.sleep(1)
+    print("Tock")
+    time.sleep(1)
